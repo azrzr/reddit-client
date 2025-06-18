cc43013867ce325828e4f084926a9ae9e952aaf6
azrzr <140966167+azrzr@users.noreply.github.com>
2025-06-18 19:29:31 +0200

    ensure that tests work correctly

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect } from "vitest";
 import App from "./App";
 
@@ -9,5 +9,20 @@ describe("App", () => {
 
         expect(text.textContent).toBe("Vite + React")
 
+    }),
+    it("initialises the counter at 0", () => {
+        render(<App />);
+        const counter = screen.getByTestId("counter").textContent;
+
+        expect(counter).toBe("0");
+    }),
+    it("increments the counter to 1 on click of the button", () => {
+        render(<App />);
+        const button = screen.getByRole("button");
+
+        fireEvent.click(button);
+        const counter = screen.getByTestId("counter").textContent;
+
+        expect(counter).toBe("1");
     })
 })
